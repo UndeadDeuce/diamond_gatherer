c0efb2697da20ac55ff4a3630337d86bb714527a
UndeadDeuce <stefaniapop.hu@gmail.com>
2020-11-11 19:38:01 +0200

    Curs 1 - Introducere html, css, javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+var name = "Paul"; //string
+var amount = 23; //numeric
+var knowJavascript = true; //booleanvar
+var fruits = ["apple", "lemon", "orange"]; //array
+
+function add() {
+    return 2 + 3;
+}
+
+var person = {
+    height: 2,
+    canWalk: function() {
+        console.log("Yes, I can walk");
+    },
+    name: "Julia"
+}
